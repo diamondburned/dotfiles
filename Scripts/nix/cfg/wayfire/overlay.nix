6c635b38ddb2b8030c22a7745a8aca94ae44882c
diamondburned <diamond@arikawa-hi.me>
2021-11-07 15:00:47 -0800

    Update

--- Scripts/nix/cfg/wayfire/overlay.nix
+++ Scripts/nix/cfg/wayfire/overlay.nix
@@ -24,6 +24,10 @@ let nixosPkgs = import <nixos> {};
 		# mesonFlags = [ "-Dwlroots:x11-backend=disabled" ];
 	});
 
+	makeGApp = pkgs: pkg: pkg.overrideAttrs(old: {
+		nativeBuildInputs = old.nativeBuildInputs ++ (with pkgs; [ wrapGAppsHook ]);
+	});
+
 in self: super: {
 	inherit (waylandPkgs) wlogout wlsunset;
 
@@ -42,70 +46,6 @@ in self: super: {
 			"$out/share/wayland-sessions/wayfire.desktop"	
 	'';
 
-	# wayfire-unwrapped = super.wayfireApplications-unwrapped.wayfire.overrideAttrs (old: {
-	# 	# version = "0.7.2";
-	# 	# src = super.fetchFromGitHub {
-	# 	# 	owner  = "WayfireWM";
-	# 	# 	repo   = "wayfire";
-	# 	# 	rev    = "7a69c1e3c0fe0cd0134db6d8fe581d9d831f648e";
-	# 	# 	sha256 = "${super.lib.fakeSha256}";
-	# 	# 	fetchSubmodules = true;
-	# 	# };
-	# 	passthru.providedSessions = [ "wayfire" ];
-	# 	postInstall = ''
-	# 		mkdir -p "$out/share/wayland-sessions"
-	# 		cp ${./wayfire.desktop} \
-	# 			"$out/share/wayland-sessions/wayfire.desktop"
-	# 	'';
-	# });
-
-	# wayfireApplications-unwrapped = super.wayfireApplications-unwrapped // {
-	# 	wayfire = self.wayfire-unwrapped;
-	# };
-	# wayfire = self.wayfire-unwrapped;
-
-	wf-shell = super.wayfirePlugins.wf-shell.overrideAttrs(old: {
-		nativeBuildInputs = old.nativeBuildInputs ++ (with super; [ wrapGAppsHook ]);
-	});
-	# wf-shell = waylandPkgs.wayfirePlugins.wf-shell;
-
-	# wf-shell = super.stdenv.mkDerivation rec {
-	# 	name = "wf-shell-0.7.0";
-	# 	src  = super.fetchFromGitHub {
-	# 		owner  = "WayfireWM";
-	# 		repo   = "wf-shell";
-	# 		rev    = "v0.7.0";
-	# 		sha256 = "1q54ivzb81wxbmyyggsi45jcbhdhilp49g8fskwyfrnvv2w021c9";
-	# 		fetchSubmodules = true;
-	# 	};
-
-	# 	enableParallelBuilding = true;
-
-	# 	nativeBuildInputs = with super; [
-	# 		meson
-	# 		ninja
-	# 		pkgconfig
-	# 	];
-
-	# 	buildInputs = with super; [
-	# 		self.wf-config
-	# 		self.wayfire
-
-	# 		wayland
-	# 		wayland-protocols
-	# 		wayland-utils
-	# 		waylandpp
-	# 		gnome3.gtkmm3.dev
-	# 		gnome3.gtk.dev
-	# 		gtk-layer-shell
-	# 		libpulseaudio
-	# 		alsaLib.dev
-	# 		graphviz
-	# 		glm
-	# 	];
-	# };
-
-# 	sway-unwrapped = super.sway-unwrapped.overrideAttrs (old: {
-# 		patches = (old.patches or []) ++ [ ../sway/raise_floating.diff ];
-# 	});
+	wf-shell = makeGApp super super.wayfirePlugins.wf-shell;
+	wayfire  = makeGApp super super.wayfire;
 }
