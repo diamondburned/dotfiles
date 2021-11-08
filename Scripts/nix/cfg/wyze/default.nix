b8b206803f354675f14166fd856bfa47466ed7cb
diamondburned <diamond@arikawa-hi.me>
2021-11-07 17:00:47 -0800

    Update

--- Scripts/nix/cfg/wyze/default.nix
+++ Scripts/nix/cfg/wyze/default.nix
@@ -4,8 +4,8 @@ let ip = "192.168.1.7";
 
 	schedule = {
 		name        = "wyze-bulb";
-		description = "Turn on the Wyze bulb everyday at 6PM";
-		calendar    = "*-*-* 17:00";
+		description = "Turn on the Wyze bulb everyday at 4:30PM";
+		calendar    = "*-*-* 16:30";
 		command     = "${pkgs.curl}/bin/curl -s 'http://${ip}/?m=1&d0=100'";
 	};
 
