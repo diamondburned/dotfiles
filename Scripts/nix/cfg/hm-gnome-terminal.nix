d921592a907859a5ccf6686aeb46a35af0b89f70
diamondburned <diamond@arikawa-hi.me>
2022-04-18 22:00:14 -0700

    Update

--- Scripts/nix/cfg/hm-gnome-terminal.nix
+++ Scripts/nix/cfg/hm-gnome-terminal.nix
@@ -11,7 +11,7 @@
 			showScrollbar = false;
 			default = true;
 
-			font	= "Inconsolata Bold 10";
+			font	= "Inconsolata Bold 11";
 			colors  = {
 				backgroundColor = "#1D1D1D";
 				foregroundColor = "#FFFFFF";
@@ -29,7 +29,7 @@
 			visibleName  = "Google Light";
 			showScrollbar = false;
 
-			font   = "Inconsolata Bold 10";
+			font   = "Inconsolata Bold 11";
 			colors = {
 				backgroundColor = "#FEFEFE";
 				foregroundColor = "#373B41";
