7c5e5ef3ae39bf11da05d992d45a7f98f07aa1d7
diamondburned <diamond@arikawa-hi.me>
2022-04-25 16:00:14 -0700

    Update

--- Scripts/nix/hardware-custom.nix
+++ Scripts/nix/hardware-custom.nix
@@ -90,8 +90,8 @@ in {
 	];
 
 	# Refer to unstable.nix.
-	# boot.kernelPackages = pkgs.linuxPackages_5_13;
-	boot.kernelPackages = pkgs.linuxPackages_xanmod;
+	boot.kernelPackages = pkgs.linuxPackages_5_16;
+	# boot.kernelPackages = pkgs.linuxPackages_xanmod;
 
 	# Kernel tweaks and such for real-time audio.
 	musnix = {
