6c41582c94c3d0de823236231f751445e98352b9
diamondburned <diamond@arikawa-hi.me>
2022-04-28 14:00:35 -0700

    Update

--- Scripts/nix/cfg/sway/default.nix
+++ Scripts/nix/cfg/sway/default.nix
@@ -30,7 +30,7 @@ in {
 	xdg.portal = {
 		enable       = true;
 		extraPortals = with pkgs; [ xdg-desktop-portal-wlr ];
-		gtkUsePortal = true;
+		gtkUsePortal = false;
 	};
 
 	nixpkgs.overlays = [
