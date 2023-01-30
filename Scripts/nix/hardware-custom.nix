3bef514fa59e1247e8a315d0885f5080e3d3a9a5
diamondburned <diamond@arikawa-hi.me>
2023-01-30 06:00:12 -0800

    Update

--- Scripts/nix/hardware-custom.nix
+++ Scripts/nix/hardware-custom.nix
@@ -274,8 +274,8 @@ in {
 		};
 	};
 
-	nix.settings.max-jobs = lib.mkForce 6;
-	nix.settings.cores = lib.mkForce 12;
+	nix.settings.max-jobs = lib.mkForce 4;
+	nix.settings.cores = lib.mkForce 6;
 
 	# These patches taken from cidkid's config.
 	# le9db patchset to prevent I/O thrasing on high memory loads.
