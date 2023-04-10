4f0204bf5e82f9266a3e0a84683d7dfbe019ef38
diamondburned <diamond@arikawa-hi.me>
2023-04-09 18:00:35 -0700

    Update

--- Scripts/nix/cfg/bluetooth-hack/default.nix
+++ Scripts/nix/cfg/bluetooth-hack/default.nix
@@ -1,37 +1,15 @@
 { config, lib, pkgs, ... }:
 
-# https://unix.stackexchange.com/questions/539762/how-do-i-suspend-sleep-while-bluetooth-is-active
-let bluetooth-sleep = pkgs.writeShellScript "bluetooth-sleep" (builtins.readFile ./bluetooth-sleep);
-	id = {
-		vendor = "8087";
-		product = "0033";
-	};
-in
-
 {
 	systemd.services."bluetooth-sleep" = {
 		enable = true;
 		description = "disable bluetooth for systemd sleep/suspend targets";
-		before = [
-			"sleep.target"
-			"suspend.target"
-			"hybrid-sleep.target"
-			"suspend-then-hibernate.target"
-		];
-		unitConfig = {
-			StopWhenUnneeded = true;
-		};
-		serviceConfig = {
-			Type = "oneshot";
-			RemainAfterExit = true;
-			ExecStart = with id; "${bluetooth-sleep} start ${vendor} ${product}";
-			ExecStop = with id; "${bluetooth-sleep} stop ${vendor} ${product}";
-		};
-		wantedBy = [
-			"sleep.target"
-			"suspend.target"
-			"hybrid-sleep.target"
-			"suspend-then-hibernate.target"
-		];
+		before = [ "sleep.target" "suspend.target" "hybrid-sleep.target" "suspend-then-hibernate.target" ];
+		stopWhenUnneeded = true;
+		type = "oneshot";
+		remainAfterExit = true;
+		serviceConfig.ExecStart = "${pkgs.rfkill}/bin/rfkill block bluetooth";
+		serviceConfig.ExecStop = "${pkgs.rfkill}/bin/rfkill unblock bluetooth";
+		wantedBy = [ "sleep.target" "suspend.target" "hybrid-sleep.target" "suspend-then-hibernate.target" ];
 	};
 }
