9092fd430843a52a0cb20593056a6c2f7a55840b
diamondburned <diamond@arikawa-hi.me>
2023-07-31 20:00:04 -0700

    Update

--- Scripts/nix/hardware-custom.nix
+++ Scripts/nix/hardware-custom.nix
@@ -126,19 +126,19 @@ in {
 
 	# Refer to unstable.nix.
 	# boot.kernelPackages = pkgs.nixpkgs_unstable.linuxKernel.packages.linux_xanmod_latest;
-	# boot.kernelPackages = pkgs.linuxPackages;
 	# boot.kernelPackages = pkgs.linuxPackages_latest;
 	# boot.kernelPackages = pkgs.linuxPackages-xanmod;
-	boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux_6_3;
-	boot.kernelPatches = [
-		{
-			name = "enable-logitech-hires-scroll";
-			patch = ./overlays/patches/linux-enable-logitech-hires-scroll.patch;
-			extraStructuredConfig = with lib.kernel; {
-				DYNAMIC_DEBUG = yes;
-			};
-		}
-	];
+	# boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux_6_3;
+	boot.kernelPackages = pkgs.linuxPackages;
+	# boot.kernelPatches = [
+	# 	{
+	# 		name = "enable-logitech-hires-scroll";
+	# 		patch = ./overlays/patches/linux-enable-logitech-hires-scroll.patch;
+	# 		extraStructuredConfig = with lib.kernel; {
+	# 			DYNAMIC_DEBUG = yes;
+	# 		};
+	# 	}
+	# ];
 
 	# Use the Linux xanmod kernel with x86-64-v3 and LTO because we're on a
 	# fucking Alder Lake CPU.
