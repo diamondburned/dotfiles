3f08b35d52518f38749f62c95afceab8e7e3178d
diamondburned <diamond@arikawa-hi.me>
2023-09-02 04:00:08 -0700

    Update

--- Scripts/nix/configuration.nix
+++ Scripts/nix/configuration.nix
@@ -754,20 +754,7 @@ in {
 			drawing
 			sticky
 			fragments
-
-			(mixxx.overrideAttrs (old:
-				if lib.versionAtLeast old.version "2.4"
-				then { }
-				else {
-					src = fetchFromGitHub {
-						owner = "mixxxdj";
-						repo = "mixxx";
-						rev = "592c0c728e538fb57a1fbcf617fed2c0dc39085d"; # 2.4-beta
-						sha256 = "sha256-/ysYBR2/oJXgz0LXdrca720O3T3WZf6GHOnyHa81EP0=";
-					};
-					version = "2.4-beta";
-				}
-			))
+			(callPackage ./overlays/packages/mixxx/beta.nix {})
 
 			# System
 			xorg.xhost # dependency for wsudo
