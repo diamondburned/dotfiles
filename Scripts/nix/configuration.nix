0a879ffebb40b045b6e0f1c58b05685980f740b4
diamondburned <diamond@arikawa-hi.me>
2023-09-02 03:00:02 -0700

    Update

--- Scripts/nix/configuration.nix
+++ Scripts/nix/configuration.nix
@@ -756,10 +756,15 @@ in {
 			fragments
 
 			(mixxx.overrideAttrs (old:
-				if lib.versionAtLeast "2.4" old.version
-				then old
-				else old.override {
-					src = <mixxx2_4-beta>;
+				if lib.versionAtLeast old.version "2.4"
+				then { }
+				else {
+					src = fetchFromGitHub {
+						owner = "mixxxdj";
+						repo = "mixxx";
+						rev = "592c0c728e538fb57a1fbcf617fed2c0dc39085d"; # 2.4-beta
+						sha256 = "sha256-/ysYBR2/oJXgz0LXdrca720O3T3WZf6GHOnyHa81EP0=";
+					};
 					version = "2.4-beta";
 				}
 			))
