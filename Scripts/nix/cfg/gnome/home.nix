d41b6285616fcc20eebe84ba051b82e93f34af88
diamondburned <diamond@arikawa-hi.me>
2023-09-27 21:00:19 -0700

    Update

--- Scripts/nix/cfg/gnome/home.nix
+++ Scripts/nix/cfg/gnome/home.nix
@@ -2,18 +2,24 @@
 
 {
 	home.packages = with pkgs; [
-			gnome-usage
-			gnome.polari
-			gnomeExtensions.vitals
-			gnomeExtensions.caffeine
-			gnomeExtensions.worksets
-			gnomeExtensions.gsconnect
-			gnomeExtensions.dash-to-panel
-			gnomeExtensions.tiling-assistant
-			gnomeExtensions.brightness-control-using-ddcutil
-			gnomeExtensions.search-light
-			gnomeExtensions.rounded-window-corners
-			gnomeExtensions.expandable-notifications
-			gnomeExtensions.notification-banner-reloaded
+		gnome-usage
+		gnome.polari
+		gnomeExtensions.vitals
+		gnomeExtensions.caffeine
+		gnomeExtensions.worksets
+		gnomeExtensions.gsconnect
+		gnomeExtensions.dash-to-panel
+		gnomeExtensions.tiling-assistant
+		gnomeExtensions.brightness-control-using-ddcutil
+		gnomeExtensions.search-light
+		gnomeExtensions.rounded-window-corners
+		gnomeExtensions.expandable-notifications
+		gnomeExtensions.notification-banner-reloaded
 	];
+
+	pam.sessionVariables = {
+		NIXOS_OZONE_WL = "1";
+		QT_QPA_PLATFORM = "wayland";
+		MOZ_ENABLE_WAYLAND = "1";
+	};
 }
