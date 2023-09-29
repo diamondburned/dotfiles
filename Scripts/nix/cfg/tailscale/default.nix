db5f63119b785c8b1f7d36a67ae3c8219366500f
diamondburned <diamond@arikawa-hi.me>
2023-09-28 23:00:31 -0700

    Update

--- Scripts/nix/cfg/tailscale/default.nix
+++ Scripts/nix/cfg/tailscale/default.nix
@@ -3,4 +3,9 @@
 {
 	services.tailscale.enable = true;
 	services.tailscale.useRoutingFeatures = "client";
+
+	networking.firewall.interfaces.tailscale0 = {
+		allowedTCPPortRanges = [ { from = 0; to = 65535; } ];
+		allowedUDPPortRanges = [ { from = 0; to = 65535; } ];
+	};
 }
