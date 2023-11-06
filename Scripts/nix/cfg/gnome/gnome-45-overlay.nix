1df15a51b3a5b9db1ecc9fc2509feab6f96dc3cd
diamondburned <diamond@arikawa-hi.me>
2023-11-06 01:00:34 -0800

    Update

--- Scripts/nix/cfg/gnome/gnome-45-overlay.nix
+++ Scripts/nix/cfg/gnome/gnome-45-overlay.nix
@@ -5,15 +5,81 @@ with super.lib;
 
 let
 	nixpkgs_gnome = import <nixpkgs_gnome> {};
+	nixpkgs = import <nixpkgs> {
+		overlays = [
+			(self: super: {
+				gtk4 = super.callPackage <nixpkgs_gnome/pkgs/development/libraries/gtk/4.x.nix> {
+					AppKit = null;
+					Cocoa = null;
+				};
+				glib = super.callPackage <nixpkgs_gnome/pkgs/development/libraries/glib> {};
+				gnome-tecla = self.callPackage <nixpkgs_gnome/pkgs/applications/misc/gnome-tecla/default.nix> {};
+			})
+			(self: super: (unfuckPkgs {
+				adwaita-icon-theme = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/adwaita-icon-theme> {};
+				gdm = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gdm> {};
+				gnome-backgrounds = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-backgrounds> {};
+				gnome-bluetooth = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-bluetooth> {};
+				gnome-color-manager = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-color-manager> {};
+				gnome-control-center = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-control-center> {};
+				gnome-common = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-common> {};
+				gnome-disk-utility = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-disk-utility> {};
+				gnome-keyring = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-keyring> {};
+				libgnome-keyring = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/libgnome-keyring> {};
+				gnome-online-miners = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-online-miners> {};
+				gnome-remote-desktop = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-remote-desktop> {};
+				gnome-session = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-session> {};
+				gnome-session-ctl = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-session/ctl.nix> {};
+				gnome-shell = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-shell> {};
+				gnome-shell-extensions = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-shell-extensions> {};
+				gnome-screenshot = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-screenshot> {};
+				gnome-settings-daemon = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-settings-daemon> {};
+				gnome-terminal = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-terminal> {};
+				gnome-themes-extra = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-themes-extra> {};
+				gnome-user-share = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-user-share> {};
+				mutter = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/mutter> {};
+				nautilus = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/nautilus> {};
+				nixos-gsettings-overrides = self.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/nixos/gsettings-overrides> {};
+			}))
+		];
+	};
 
 	unfuckPkgs = pkgs: mapAttrs
-		(_: pkg:
-			((pkg.override { inherit (nixpkgs_gnome) glib; }).overrideAttrs (_: { doCheck = false; })))
-
+		(_: pkg: pkg.overrideAttrs (_: { doCheck = false; }))
 		(pkgs);
+
+	pkgPath = pkg: builtins.head (builtins.split ":" (pkg.meta.position));
 in
 
 {
+	gnome = super.gnome.overrideScope' (gnomeself: gnomesuper: {
+		inherit (nixpkgs.gnome)
+			adwaita-icon-theme
+			gdm
+			gnome-backgrounds
+			gnome-bluetooth
+			gnome-color-manager
+			gnome-control-center
+			gnome-common
+			gnome-disk-utility
+			gnome-keyring
+			libgnome-keyring
+			gnome-online-miners
+			gnome-remote-desktop
+			gnome-session
+			gnome-session-ctl
+			gnome-shell
+			gnome-shell-extensions
+			gnome-screenshot
+			gnome-settings-daemon
+			gnome-terminal
+			gnome-themes-extra
+			gnome-user-share
+			mutter
+			nautilus
+			nixos-gsettings-overrides;
+	});
+
 	# Override with GNOME 45 builds.
 	# gnome = super.gnome.overrideScope' (gnomeself: gnomesuper: {
 	# 	inherit (nixpkgs_gnome.gnome)
@@ -52,34 +118,4 @@ in
 	# 		mutter
 	# 		nautilus;
 	# });
-
-	gnome = super.gnome.overrideScope (gnomeself: gnomesuper: (unfuckPkgs {
-		adwaita-icon-theme = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/adwaita-icon-theme> {};
-		gdm = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gdm> {};
-		gnome-backgrounds = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-backgrounds> {};
-		gnome-bluetooth = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-bluetooth> {};
-		gnome-color-manager = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-color-manager> {};
-		gnome-control-center = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-control-center> {};
-		gnome-common = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-common> {};
-		gnome-disk-utility = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-disk-utility> {};
-		gnome-keyring = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-keyring> {};
-		libgnome-keyring = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/libgnome-keyring> {};
-		gnome-online-miners = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-online-miners> {};
-		gnome-remote-desktop = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-remote-desktop> {};
-		gnome-session = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-session> {};
-		gnome-session-ctl = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-session/ctl.nix> {};
-		gnome-shell = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-shell> {};
-		gnome-shell-extensions = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-shell-extensions> {};
-		gnome-screenshot = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-screenshot> {};
-		gnome-settings-daemon = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-settings-daemon> {};
-		gnome-terminal = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-terminal> {};
-		gnome-themes-extra = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-themes-extra> {};
-		gnome-user-share = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/gnome-user-share> {};
-		mutter = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/mutter> {};
-		nautilus = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/core/nautilus> {};
-		nixos-gsettings-overrides = gnomeself.callPackage <nixpkgs_gnome/pkgs/desktops/gnome/nixos/gsettings-overrides> {};
-
-		inherit (nixpkgs_gnome)
-			gnome-tecla;
-	}));
 }
