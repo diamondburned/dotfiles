4265ad9545f487b4165dd79af5ed436acab7c1e4
diamondburned <diamond@arikawa-hi.me>
2024-01-08 21:02:43 -0800

    Update

--- Scripts/nix/machines/lilyhoshii/base/configuration.nix
+++ Scripts/nix/machines/lilyhoshii/base/configuration.nix
@@ -68,7 +68,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.diamond = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       firefox
       tree
