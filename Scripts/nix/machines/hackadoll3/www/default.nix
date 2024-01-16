5309c04c2ed841e1ac046b9637adf512226114b6
diamondburned <diamond@arikawa-hi.me>
2024-01-16 03:00:54 -0800

    Update

--- Scripts/nix/machines/hackadoll3/www/default.nix
+++ Scripts/nix/machines/hackadoll3/www/default.nix
@@ -108,9 +108,19 @@ in
 				'';
 			${subdomain "esp"} =
 				let
-					index = pkgs.writeTextDir "index.html" ''
+					index = pkgs.writeText "index.html" ''
 						<!DOCTYPE html>
 						<title>Tasmota Index</title>
+						<meta charset="utf-8">
+						<meta name="viewport" content="width=device-width, initial-scale=1">
+						<link rel="icon" type="image/png" href="/favicon.png">
+						<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css">
+						<link rel="stylesheet" href="https://markdowncss.github.io/retro/css/retro.css">
+						<style>
+							body {
+								font-family: monospace !important;
+							}
+						</style>
 
 						<h1>Tasmota Index</h1>
 						<ul>
@@ -118,13 +128,17 @@ in
 							<li><a href="/plug">Tasmota Plug</a></li>
 						</ul>
 					'';
+					fs = pkgs.runCommandLocal "esp-fs" {
+					} ''
+						mkdir -p $out
+						ln -s ${index} $out/index.html
+						ln -s ${<dotfiles/static/tasmota-white.png>} $out/favicon.png
+					'';
 				in ''
 					redir /bulb http://192.168.2.124:80 302
 					redir /plug http://192.168.2.238:80 302
-					handle / {
-						file_server
-						root * ${index}
-					}
+					root * ${fs}
+					file_server
 				'';
 		};
 	};
