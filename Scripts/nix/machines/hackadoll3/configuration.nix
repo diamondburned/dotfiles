35bb046c7da822b2b0972c040c446789707578aa
diamondburned <diamond@libdb.so>
2024-02-24 18:00:43 -0800

    Update

--- Scripts/nix/machines/hackadoll3/configuration.nix
+++ Scripts/nix/machines/hackadoll3/configuration.nix
@@ -482,6 +482,7 @@ in {
 			<dotfiles/cfg/hm-blackbox-terminal.nix>
 			<dotfiles/cfg/hm-gnome-terminal.nix>
 			<dotfiles/cfg/hm-alacritty.nix>
+			<dotfiles/cfg/git/home.nix>
 			<dotfiles/cfg/gtk/home.nix>
 			<dotfiles/cfg/nvim/home.nix>
 			<dotfiles/cfg/gnome/home.nix>
@@ -505,36 +506,6 @@ in {
 			enable = true;
 			config = { load_dotenv = false; };
 			nix-direnv.enable = true;
-		};
-
-		programs.git = {
-			enable = true;
-			userName  = "diamondburned";
-			userEmail = "diamond@arikawa-hi.me";
-			signing = {
-				key = "D78C4471CE776659";
-				signByDefault = true;
-			};
-			lfs.enable = true;
-			extraConfig = {
-				http = {
-					cookieFile = "/home/diamond/.gitcookies";
-				};
-				core = {
-					excludesfile = "${pkgs.writeText "gitignore" (
-						builtins.concatStringsSep "\n" [
-							".envrc"
-							".direnv"
-						]
-					)}";
-				};
-				url = {
-					"ssh://git@github.com/" = { insteadOf = "https://github.com/"; };
-					"ssh://git@gitlab.com/" = { insteadOf = "https://gitlab.com/"; };
-				};
-				pull.rebase = true;
-				push.autoSetupRemote = true;
-			};
 		};
 
 		programs.bash = {
