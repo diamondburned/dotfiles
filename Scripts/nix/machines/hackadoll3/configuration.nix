7a03ddffcdb7c558b5443de5bc152bf9cdb9cb61
diamondburned <diamond@libdb.so>
2024-03-18 16:00:30 -0700

    Update

--- Scripts/nix/machines/hackadoll3/configuration.nix
+++ Scripts/nix/machines/hackadoll3/configuration.nix
@@ -280,7 +280,7 @@ in {
 	# Some programs need SUID wrappers, can be configured further or are
 	# started in user sessions.
 	programs.mtr.enable = true;
-	programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+	programs.gnupg.agent = { enable = true; };
 
 	# List services that you want to enable:
 
