d2ffd37f1e22a71eae3a4beb8b4f3274a92ca449
diamondburned <diamond@libdb.so>
2024-06-03 17:00:14 -0700

    Update

--- Scripts/nix/cfg/v4l2/default.nix
+++ Scripts/nix/cfg/v4l2/default.nix
@@ -1,5 +1,12 @@
 { config, lib, pkgs, ... }:
 
+let
+	cameraDevice = {
+		idVendor = "04a9";
+		idProduct = "3270";
+	};
+in
+
 {
 	boot.kernelModules = [ "v4l2loopback" ];
 	boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
@@ -8,7 +15,15 @@
 			''devices=3''
 			''video_nr=10,11,12''
 			''exclusive_caps=1,1,1''
-			''card_label="Camera Loopback 1","Camera Loopback 2","Camera Loopback 3"''
+			''card_label="Camera Loopback 1,Camera Loopback 2,Camera Loopback 3"''
 		]}
 	'';
+
+	environment.systemPackages = with pkgs; [
+		config.boot.kernelPackages.v4l2loopback
+		gnome.cheese
+	];
+
+	# Canon DSLR configuration.
+	# services.udev.extraRules = 
 }
---
+++ Scripts/nix/cfg/v4l2/dslr.nix
@@ -0,0 +1,52 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with builtins;
+
+let
+	self = config.hardware.v4l2-dslr;
+in
+
+{
+	options.hardware.v4l2-dslr = {
+		enable = mkEnableOption "Bridge a physical DSLR camera to a V4L2 loopback device";
+
+		udevRule = mkOption {
+			type = types.str;
+			example = "ATTRS{idVendor}==\"046d\", ATTRS{idProduct}==\"0825\"";
+			description = "The udev rule to match the camera";
+		};
+
+		videoDevice = mkOption {
+			type = types.str;
+			example = "/dev/video1";
+			description = "The V4L2 loopback device to use";
+		};
+	};
+
+	config = lib.mkIf self.enable {
+		systemd.services.v4l2-dslr = {
+			description = "V4L2 loopback bridge for DSLR cameras";
+			script = ''
+				set -eo pipefail
+				gphoto2 --stdout --capture-movie | \
+				ffmpeg -vcodec rawvideo -pix_fmt yuv420p -f v4l2 /dev/video0
+			'';
+			path = with pkgs; [
+				gphoto2
+				ffmpeg
+			];
+			serviceConfig = {
+				Type = "simple";
+				ExecStart = "${pkgs.v4l2loopback}/bin/v4l2loopback -r 1 -p 1 -f 30 -d /dev/video0 -i /dev/video1";
+				Restart = "always";
+				RestartSec = 5;
+			};
+		};
+
+		services.udev.extraRules = ''
+			# ${self.usbRule}
+			SUBSYSTEM=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="0825", TAG+="systemd", ENV{SYSTEMD_WANTS}+="v4l2-dslr.service"
+		'';
+	};
+}
