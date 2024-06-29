d7860ad0c4056c9a6beff691c2ae7d435cf01cff
diamondburned <diamond@libdb.so>
2024-06-29 10:00:09 -0700

    Update

--- Scripts/nix/machines/lilyhoshii/base/configuration.nix
+++ Scripts/nix/machines/lilyhoshii/base/configuration.nix
@@ -19,9 +19,6 @@
   networking.hostName = "lilyhoshii";
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
