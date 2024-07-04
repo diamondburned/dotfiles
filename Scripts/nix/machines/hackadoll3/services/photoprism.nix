3eaaf0aa425668911b25837d96e4c435b4627307
diamondburned <diamond@libdb.so>
2024-07-04 17:00:58 +0700

    Update

--- Scripts/nix/machines/hackadoll3/services/photoprism.nix
+++ Scripts/nix/machines/hackadoll3/services/photoprism.nix
@@ -2,7 +2,6 @@
 
 let
 	drivePath = "/run/media/diamond/Tertiary";
-	driveService = "run-media-diamond-Tertiary.mount";
 in
 
 {
@@ -17,12 +16,13 @@ in
 			PHOTOPRISM_ADMIN_USER = "diamond";
 			PHOTOPRISM_USERS_PATH = "Photoprism";
 			PHOTOPRISM_PASSWORD_LENGTH = "0";
-			PHOTOPRISM_READONLY = "1";
-			PHOTOPRISM_DETECT_NSFW = "1";
-			PHOTOPRISM_UPLOAD_NSFW = "1";
+			PHOTOPRISM_READONLY = "true";
+			PHOTOPRISM_DETECT_NSFW = "true";
+			PHOTOPRISM_UPLOAD_NSFW = "true";
 			PHOTOPRISM_APP_NAME = "Photoprism";
 			PHOTOPRISM_APP_MODE = "minimal-ui";
 			PHOTOPRISM_APP_COLOR = "#F5A9B8";
+			PHOTOPRISM_HTTP_MODE = "release";
 			PHOTOPRISM_SITE_URL = "http://photoprism/";
 			PHOTOPRISM_SITE_TITLE = "Photoprism";
 			PHOTOPRISM_SITE_DESCRIPTION = "Hosted on ${config.networking.hostName}.";
@@ -31,31 +31,47 @@ in
 			PHOTOPRISM_JPEG_QUALITY = "91";
 			PHOTOPRISM_JPEG_SIZE = "1920";
 			PHOTOPRISM_PNG_SIZE = "1920";
+			PHOTOPRISM_DEBUG = "true";
+			PHOTOPRISM_LOG_LEVEL = "debug";
 		};
 	};
 
-	# systemd.services.photoprism-mount-init = {
-	# 	serviceConfig = {
-	# 		Type = "oneshot";
-	# 		ReadWritePaths = [ drivePath ];
-	# 	};
-	# 	script = ''
-	# 		set -ex
-	# 		for d in \
-	# 			${drivePath}/.photoprism \
-	# 			${drivePath}/.photoprism/{storage,database}; do
-	#
-	# 			[[ -d "$d" ]] || mkdir "$d"
-	# 		done
-	# 	'';
-	# 	# Ensure that the Photoprism service starts after the drive is mounted and that it does not
-	# 	# start if the drive is not mounted.
-	# 	requiredBy = [ "photoprism.service" ];
-	# 	before = [ "photoprism.service" ];
-	# 	# Require the drive to be mounted.
-	# 	requires = [ driveService ];
-	# 	after = [ driveService ];
-	# };
+	systemd.services.photoprism = {
+		serviceConfig = {
+			# We already manually create the user and group.
+			User = lib.mkForce "photoprism";
+			Group = lib.mkForce "users";
+			UMask = lib.mkForce "0006";
+			DynamicUser = lib.mkForce false;
+		};
+	};
+
+	systemd.services.photoprism-init = {
+		serviceConfig = {
+			Type = "oneshot";
+			UMask = "0006";
+		};
+		unitConfig = {
+			RequiresMountsFor = [ drivePath ];
+		};
+		before = [ "photoprism.service" ];
+		requiredBy = [ "photoprism.service" ];
+		script = ''
+			set -ex
+			baseDir=${lib.escapeShellArg drivePath}/.photoprism
+			if [[ ! -d "$baseDir" ]]; then
+				mkdir "$baseDir"
+				chown -R photoprism:users "$baseDir"
+			fi
+		'';
+	};
+
+	users.users.photoprism = {
+		group = "users";
+		home = "${drivePath}/.photoprism";
+		createHome = true;
+		isNormalUser = true;
+	};
 
 	# Enable http://photoprism via Tailscale.
 	diamond.tailnet-services.photoprism.localPort = config.services.photoprism.port;
