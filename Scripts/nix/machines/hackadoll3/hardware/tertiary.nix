1edbe37c017e5e0f13c207ab57d2df99e279baed
diamondburned <diamond@libdb.so>
2024-07-15 23:00:43 +0700

    Update

--- Scripts/nix/machines/hackadoll3/hardware/tertiary.nix
+++ Scripts/nix/machines/hackadoll3/hardware/tertiary.nix
@@ -5,11 +5,11 @@
 		device = "/dev/disk/by-uuid/3479fa6a-2c35-4341-a787-28103626fa21";
 		fsType = "auto";
 		options = [
+			"user"
 			"nodev"
 			"nosuid"
 			"nofail"
 			"noatime"
-			"autodefrag"
 			"x-gvfs-show"
 			"x-gvfs-name=Tertiary"
 		];
