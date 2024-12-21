56d35fb3b25047540d6c34e2efee201098483f94
diamondburned <diamond@libdb.so>
2024-12-21 13:00:49 -0800

    Update

---
+++ Scripts/nix/machines/hackadoll3/hardware/disko/4tb-nvme.nix
@@ -0,0 +1,66 @@
+{
+  disko.devices = {
+    disk = {
+      my-disk = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "512M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "main";
+                settings = {
+                  allowDiscards = true;
+                };
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        # "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = [
+                        # "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    "/swapfile" = {
+                      mountpoint = "/var/swapfile";
+                      swap.swapfile.size = "32G";
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
--- Scripts/nix/shell.nix
+++ Scripts/nix/shell.nix
@@ -1,53 +1,53 @@
 {
-	pkgs ? import <nixpkgs> {
-		overlays = [
-			(import ./overlays/gomod2nix.nix)
-		];
-	}
+  pkgs ? import <nixpkgs> {
+    overlays = [
+      (import ./overlays/gomod2nix.nix)
+    ];
+  },
 }:
 
 let
-	machines =
-		with pkgs.lib;
-		mapAttrsToList
-			(name: type: removeSuffix ".toml" name)
-			(filterAttrs
-				(name: type: hasSuffix ".toml" name)
-				(builtins.readDir ./.));
+  machines =
+    with pkgs.lib;
+    mapAttrsToList (name: type: removeSuffix ".toml" name) (
+      filterAttrs (name: type: hasSuffix ".toml" name) (builtins.readDir ./.)
+    );
 
-	lib = pkgs.lib;
+  lib = pkgs.lib;
 
-	nixPath = lib.concatStringsSep ":" [
-		"/home/diamond/.nix-defexpr/channels"
-		"/nix/var/nix/profiles/per-user/root/channels"
-		"dotfiles=${builtins.toString ./.}"
-		"nixos-config=${builtins.toString ./.}/configuration.nix"
-	];
+  nixPath = lib.concatStringsSep ":" [
+    "/home/diamond/.nix-defexpr/channels"
+    "/nix/var/nix/profiles/per-user/root/channels"
+    "dotfiles=${builtins.toString ./.}"
+    "nixos-config=${builtins.toString ./.}/configuration.nix"
+  ];
 in
 
 pkgs.mkShell {
-	buildInputs = with pkgs; [
-		# bonito
-		niv
-		git
-		git-crypt
-		gomod2nix
-		nix-output-monitor
-		lua-language-server
+  buildInputs = with pkgs; [
+    # bonito
+    disko
+    niv
+    git
+    git-crypt
+    gomod2nix
+    nixfmt-rfc-style
+    nix-output-monitor
+    lua-language-server
 
-		(writeShellScriptBin "switch" ''
-			export NIX_PATH=${lib.escapeShellArg nixPath}
-			sudo nixos-rebuild --log-format internal-json -v "$@" switch |& nom --json
-		'')
-	];
+    (writeShellScriptBin "switch" ''
+      			export NIX_PATH=${lib.escapeShellArg nixPath}
+      			sudo nixos-rebuild --log-format internal-json -v "$@" switch |& nom --json
+      		'')
+  ];
 
-	shellHook = ''
-		if [[ $HOSTNAME == "nixos" ]]; then
-			echo "You probably want to manually set your \$HOSTNAME."
-		else
-			export HOSTNAME
-		fi
-		export NIX_PATH=${lib.escapeShellArg nixPath}
-		export MACHINES="${builtins.concatStringsSep " " machines}"
-	'';
+  shellHook = ''
+    		if [[ $HOSTNAME == "nixos" ]]; then
+    			echo "You probably want to manually set your \$HOSTNAME."
+    		else
+    			export HOSTNAME
+    		fi
+    		export NIX_PATH=${lib.escapeShellArg nixPath}
+    		export MACHINES="${builtins.concatStringsSep " " machines}"
+    	'';
 }
