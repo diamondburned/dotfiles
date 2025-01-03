3a501fe8bc4396805b242d873ec0707e9f6c8197
diamondburned <diamond@libdb.so>
2025-01-03 06:00:59 -0800

    Update

--- Scripts/nix/machines/hackadoll3/hardware/scarlett.nix
+++ Scripts/nix/machines/hackadoll3/hardware/scarlett.nix
@@ -24,7 +24,7 @@ in
 
   services.pipewire.extraConfig = {
     pipewire = {
-      "10-scarlett" = {
+      "99-scarlett" = {
         "context.properties" = {
           "default.clock.quantum" = 512;
           "default.clock.min-quantum" = 32;
@@ -35,7 +35,7 @@ in
       };
     };
     pipewire-pulse = {
-      "10-scarlett" = {
+      "99-scarlett" = {
         "stream.properties" = {
           "node.latency" = "512/48000"; # default
           "resample.quality" = 10;
@@ -46,21 +46,38 @@ in
     };
   };
 
-  services.pipewire.wireplumber.extraConfig = {
-    "10-scarlett" = {
-      "monitor.alsa.rules" = [
+  services.pipewire.wireplumber.configPackages = [
+    (pkgs.writeTextDir "share/wireplumber/main.lua.d/99-scarlett.lua" ''
+      alsa_monitor.rules = {
         {
-          matches = [
-            {
-              "node.name" = "alsa_output.usb-Focusrite_Scarlett_4i4_4th_Gen_S4JQAWG4884F75-00.*";
-            }
-          ];
-          actions.update-props = {
-            "audio.format" = "S24LE";
-            "audio.allowed-rates" = allowedSampleRates;
-          };
-        }
-      ];
-    };
-  };
+          matches = {{{ "node.name", "matches", "~alsa_output.usb-Focusrite_Scarlett_4i4_4th_Gen_S4JQAWG4884F75-00.*" }}};
+          apply_properties = {
+            ["audio.format"] = "S24LE",
+            ["audio.rate"] = 48000,
+            ["audio.allowed-rates"] = { 44100, 48000, 88200, 96000, 176400, 192000 },
+            ["api.alsa.period-num"] = 2,
+            ["api.alsa.period-size"] = 2,
+          },
+        },
+      }
+    '')
+  ];
+
+  # services.pipewire.wireplumber.extraConfig = {
+  #   "99-scarlett" = {
+  #     "monitor.alsa.rules" = [
+  #       {
+  #         matches = [
+  #           {
+  #             "node.name" = "alsa_output.usb-Focusrite_Scarlett_4i4_4th_Gen_S4JQAWG4884F75-00.*";
+  #           }
+  #         ];
+  #         actions.update-props = {
+  #           "audio.format" = "S24LE";
+  #           "audio.allowed-rates" = allowedSampleRates;
+  #         };
+  #       }
+  #     ];
+  #   };
+  # };
 }
